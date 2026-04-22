e843931785d9d72629a3889c7be5783fbc6ff81c
LK <129121307+lucasnoelgb@users.noreply.github.com>
2026-04-22 20:25:25 -0300

    update de creditos

--- Simulador de Meio de Pagamento (SMM)/main.py
+++ Simulador de Meio de Pagamento (SMM)/main.py
@@ -16,34 +16,71 @@ def opcoes_compra(meio_pagamento, pagamento, data_atual):
     if meio_pagamento == "0":
         mdr = pagamento * 0.01
         liquido = pagamento - mdr
+
         data_credito = data_compra(data_atual + timedelta(days=1))
 
         print(repetidor)
-        print(f"Data da compra:{data_atual.strftime('%d/%m/%y')}")
+        print(f"Data da Compra:{data_atual.strftime('%d/%m/%y')}")
         print(f"Hora da Compra: {data_atual.strftime('%H:%M:%S')}")
-        print("Meio de pagamento: 0 – Cartão de débito")
-        print(f"Valor da compra:R$", pagamento)
+        print("Meio de Pagamento: 0 – Cartão de débito")
+        print(f"Valor da Compra:R$", pagamento)
         print(f"Valor do MDR:(taxa de transação): R$", mdr)
-        print("Valor liquído:", liquido)
-        print(f"Data de crédito: {data_credito.strftime('%d/%m/%y')}")
+        print("Valor Liquído:", liquido)
+        print(f"Data de Crédito: {data_credito.strftime('%d/%m/%y')}")
 
 
     elif meio_pagamento == "1":
+        mdr = pagamento * 0.05
+        liquido = pagamento - mdr
+        data_credito = data_compra(data_atual + timedelta(days=1))
+
         print(repetidor)
-        print(f"Hora da Compra")
-        print("Meio de pagamento: 1 – Cartão de crédito à vista")
-        print(f"Valor da compra:R$", pagamento)
-        print(f"Valor do MDR:(taxa de transação): R$")
-        print("Valor liquído: ")
-
-
+        print(f"Data da Compra: {data_atual.strftime('%d/%m/%y')}")
+        print(f"Hora da Compra: {data_atual.strftime('%H:%M:%S')}")
+        print("Meio de Pagamento: 1 – Cartão de crédito à vista")
+        print(f"Valor da Compra: R$", pagamento)
+        print(f"Valor do MDR: R$", mdr)
+        print("Valor Liquído: R$", liquido)
 
 
 
 
 
     elif meio_pagamento == "2":
-        print("Cartão de Crédito Parcelado")
+        qts_parcelas = input("Digite a Quantidade de Parcelas (2 ou 3 Parcelas): ")
+        mdr = pagamento * 0.05
+        liquido = pagamento - mdr
+        parcela = liquido / 2
+        data_credito = data_compra(data_atual + timedelta(days=1))
+
+        if qts_parcelas == "2":
+
+            print(repetidor)
+            print(f"Data da Compra: {data_atual.strftime('%d/%m/%y')}")
+            print(f"Hora da Compra: {data_atual.strftime('%H:%M:%S')}")
+            print("Meio de Pagamento: 1 – Cartão de crédito à vista")
+            print(f"Valor da Compra: R$", pagamento)
+            print(f"Valor do MDR: R$", mdr)
+            print("Valor Liquído: R$", liquido)
+            print()
+            print(parcela,"1 / 2")
+            print
+
+
+
+        elif qts_parcelas == "3":
+
+            print(repetidor)
+            print(f"Data da Compra: {data_atual.strftime('%d/%m/%y')}")
+            print(f"Hora da Compra: {data_atual.strftime('%H:%M:%S')}")
+            print("Meio de Pagamento: 1 – Cartão de crédito à vista")
+            print(f"Valor da Compra: R$", pagamento)
+            print(f"Valor do MDR: R$", mdr)
+            print("Valor Liquído: R$", liquido)
+
+
+        else:
+            print("Invalida")
 
 
 def menu_inicial(data_hora):
@@ -72,12 +109,17 @@ while True:
     elif meio_pagamento in ("0", "1", "2"):
         pagamento = input("Digite o valor da compra R$ ")
         pagamento = int(pagamento)
-        opcoes_compra(meio_pagamento, pagamento, data_atual)
+
+        if pagamento > 0:
+            opcoes_compra(meio_pagamento, pagamento, data_atual)
+
+        else:
+            print("Meio de Valor Invalido")
+
 
 
     else:
         print("Meio de Pagamento Invalido")
-
 
 
 
